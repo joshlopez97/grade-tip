9ff450db4abc1e3718268b72326cc92f7155082a
Josh Lopez <lopezja4@uci.edu>
2019-08-30 18:40:25 -0700

    Rename modules

--- GradeTip/user/__init__.py
+++ GradeTip/user/__init__.py
@@ -1,6 +1,6 @@
-from GradeTip.user.users import UserFactory
+from GradeTip.user.user import UserFactory
 from GradeTip.redis import redis_manager
-from GradeTip.user.sessions import SessionManager
+from GradeTip.user.session import SessionManager
 from GradeTip.user.username import UsernameGenerator
 
 user_factory = UserFactory(redis_manager)
